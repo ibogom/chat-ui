8b156f6a83ff57d23e8c45f2b233ba2f4d54df01
Ievgen <ievgen.bogomolov@gmail.com>
2018-01-28 18:59:51 +0200

    Validation class implementation

--- src/js/components/login/login.js
+++ src/js/components/login/login.js
@@ -3,20 +3,28 @@ import PropTypes from 'prop-types';
 
 import styles from './login.css';
 
+import ValidationUtil from '../../utils/validation.util';
+
 export default class LoginArea extends React.Component {
 
-    static propTypes = {};
+    static propTypes = {
+        login: PropTypes.func.isRequired
+    };
 
     constructor(props) {
         super(props);
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.showError = this.showError.bind(this);
+        this.sendUsername = this.sendUsername.bind(this);
+
+        this.validation = new ValidationUtil();
 
         this.state = {
             value: '',
             error: ''
         };
-
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     handleChange(e){
@@ -28,24 +36,46 @@ export default class LoginArea extends React.Component {
 
     handleSubmit(e){
         e.preventDefault();
-        if(this.state.value !== ''){
-            this.setState({
-                value: ''
-            });
-            this.props.login(this.state.value);
-        } else {
-            this.setState({
-                error: 'Username field could not be empty!'
-            });
-        }
+
+        this.validation.validate({
+            value: this.state.value,
+            field: 'username'
+        })
+            .then(this.sendUsername)
+            .catch(this.showError);
+    }
+
+    sendUsername(){
+        this.props.login(this.state.value);
+    }
+
+    showError(error){
+        this.setState({
+            error
+        });
+    }
+
+    componentWillUnmount(){
+        this.setState({
+            value: '',
+            error: ''
+        });
     }
 
     render() {
+        const isFromValid = this.state.error === '';
         return (<div className={styles["chat-login-area"]}>
             <form onSubmit={this.handleSubmit} className={styles.wrapper}>
                 <div className="logo"/>
-                <h5 className={styles.intro}>Welcome to the SpotIM chat. Please Enter your chat name to start chatting</h5>
-                <input onChange={this.handleChange} type="text" placeholder="Please enter your chat name" autoFocus/>
+                {!isFromValid ?
+                    <div className='error-message'>{this.state.error}</div> :
+                    <h5 className={styles.intro}>
+                        Welcome to the SpotIM chat. Please Enter your chat name to start chatting
+                    </h5>
+                }
+                <input onChange={this.handleChange}
+                       className={!isFromValid ? 'invalid' : ''}
+                       type="text" placeholder="Please enter your chat name" autoFocus/>
                 <input type="submit" className='btn-scs' value='JOIN'/>
             </form>
         </div>);
--- src/js/components/textarea/textarea.js
+++ src/js/components/textarea/textarea.js
@@ -3,58 +3,79 @@ import PropTypes from 'prop-types';
 
 import styles from './textarea.css'
 
+import ValidationUtil from '../../utils/validation.util';
+
 export default class Textarea extends React.Component {
 
     static propTypes = {
-
+        avatar: PropTypes.string.isRequired,
+        correlationId: PropTypes.string.isRequired,
+        sendMessage: PropTypes.func.isRequired
     };
 
     constructor(props) {
         super(props);
+        this.validation = new ValidationUtil();
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.showError = this.showError.bind(this);
+        this.sendMessage = this.sendMessage.bind(this);
+        this.setDefaultState = this.setDefaultState.bind(this);
 
         this.state = {
             value: '',
             error: ''
         };
-
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    handleChange(e){
+    handleChange(e) {
         this.setState({
             value: e.target.value,
             error: ''
         });
     }
 
-    handleSubmit(e){
+    handleSubmit(e) {
         e.preventDefault();
-        if(this.state.value !== ''){
-            this.setState({
-                value: ''
-            });
-            this.props.sendMessage({
-                username: this.props.username,
-                avatar: this.props.avatar,
-                correlationId: this.props.correlationId,
-                text: this.state.value
-            });
-        } else {
-            this.setState({
-                error: 'Message field could not be empty!'
-            });
-        }
+
+        this.validation.validate({
+            value: this.state.value,
+            field: 'message'
+        }).then(this.sendMessage).then(this.setDefaultState).catch(this.showError);
+    }
+
+    sendMessage() {
+        this.props.sendMessage({
+            username: this.props.username,
+            avatar: this.props.avatar,
+            correlationId: this.props.correlationId,
+            text: this.state.value
+        });
+    }
+
+    showError(error) {
+        this.setState({
+            error
+        });
+    }
+
+    setDefaultState(){
+        this.setState({
+            value: '',
+            error: ''
+        });
     }
 
     render() {
         const isFromValid = this.state.error === '';
-        return (<div className={styles['user-text-area'] + ' ' + (!isFromValid? styles.invalid : '')}>
+        return (<div className={styles['user-text-area'] + ' ' + (!isFromValid ? styles.invalid : '')}>
             <form onSubmit={this.handleSubmit} className={styles.wrapper}>
-                {!isFromValid ? <div className={styles['error-message']}>{this.state.error}</div> : null}
+                {!isFromValid ? <div className='error-message'>{this.state.error}</div> : null}
                 <input type="text"
-                       className={!isFromValid ? styles.invalid : ''}
+                       className={!isFromValid ? 'invalid' : ''}
                        placeholder="Please enter your message"
+                       name="message"
                        value={this.state.value}
                        onChange={this.handleChange}/>
                 <input type="submit" className='btn-scs' value="SEND"/>
