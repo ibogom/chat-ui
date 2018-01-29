6641ca766cf9a82a1dffbba3ef844156202517ed
Ievgen <ievgen.bogomolov@gmail.com>
2018-01-29 10:09:48 +0200

    Add express server

---
+++ server/bin/www.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const favicon = require('serve-favicon');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+const path = require('path');
+
+const webpack = require('webpack');
+const webpackConfig = require('../../webpack.config');
+const webpackDevMiddleWare = require('webpack-dev-middleware');
+const webpackHotMiddleware = require('webpack-hot-middleware');
+
+const compiler = webpack(webpackConfig);
+
+const NODE_ENV = process.env.NODE_ENV || 'development';
+const PORT = process.env.PORT || '3000';
+
+
+const app = express();
+
+app.set('port', PORT);
+
+if(NODE_ENV === "development") {
+    app.use(webpackDevMiddleWare(compiler, {
+        publicPath: webpackConfig.output.publicPath,
+        stats: {
+            colors: true,
+            chunks: false,
+            'errors-only': true
+        }
+    }));
+}
+
+app.use(webpackHotMiddleware(compiler, {
+    log: console.log
+}));
+
+
+app.use(favicon(path.join(__dirname, '../../public', 'favicon.ico')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(cookieParser());
+
+app.use(express.static(path.join(__dirname, '../../public')));
+
+const server = app.listen(app.get('port'), (err) => {
+    if(err){
+        return console.error(err);
+    }
+    console.log('Node server started from ' + path.join(__dirname, '../../public') + ' on http://localhost' + app.get('port'));
+});
