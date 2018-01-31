0b804a05440b35acfcadd9c6cd087e1af7d0a32f
Ievgen <ievgen.bogomolov@gmail.com>
2018-01-31 21:45:38 +0200

    Redux dev tool implementation

--- src/js/stores/index.js
+++ src/js/stores/index.js
@@ -4,6 +4,8 @@ import thunk from 'redux-thunk';
 /** REDUCERS **/
 import AppReducers from '../reducers';
 
-const store = compose(applyMiddleware(thunk))(createStore)(AppReducers);
+const store = createStore(AppReducers,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    compose(applyMiddleware(thunk)));
 
 export default store;
